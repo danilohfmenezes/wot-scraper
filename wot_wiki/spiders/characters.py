f0be736c6c07e799a4b00b55cc324b05d6eccbe9
Danilo Henrique Fonseca Menezes <dunderdanilo@gmail.com>
2020-04-10 14:43:15 -0300

    :sparkles: Split first_mentioned, first_appeared and last_appeared in two columns each. Book, chapter.

--- wot_wiki/spiders/characters.py
+++ wot_wiki/spiders/characters.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CharactersSpider(scrapy.Spider):
@@ -10,15 +11,25 @@ class CharactersSpider(scrapy.Spider):
                   'https://wot.fandom.com/wiki/El%27Nynaeve_ti_al%27Meara_Mandragoran']
 
     def parse(self, response):
+        first_metioned = self.splitBookChapter(
+            self.getAttribute(response, 'mentioned'))
+        first_appeared = self.splitBookChapter(
+            self.getAttribute(response, 'appeared'))
+        last_appeared = self.splitBookChapter(
+            self.getAttribute(response, 'lastappeared'))
+
         yield {
             'name': self.getAttribute(response, 'name'),
             'nationality': self.getAttribute(response, 'nationality'),
             'gender': self.getAttribute(response, 'gender'),
             'hair_color': self.getAttribute(response, 'hair'),
             'eye_color': self.getAttribute(response, 'eyes'),
-            'first_mentioned': self.getAttribute(response, 'mentioned'),
-            'first_appeared': self.getAttribute(response, 'appeared'),
-            'last_appeared': self.getAttribute(response, 'lastappeared'),
+            'first_mentioned_book': first_metioned['book'],
+            'first_mentioned_chapter': first_metioned['chapter'],
+            'first_appeared_book': first_appeared['book'],
+            'first_appeared_chapter': first_appeared['chapter'],
+            'last_appeared_book': last_appeared['book'],
+            'last_appeared_chapter': last_appeared['chapter'],
             'ajah': self.getAttribute(response, 'ajah')
         }
 
@@ -35,3 +46,17 @@ class CharactersSpider(scrapy.Spider):
                     f'.//descendant::*[@data-source="{attribute}"]/div/text()').get()
 
             return attr
+
+    def splitBookChapter(self, attribute):
+        if attribute is None:
+            return {
+                'book': None,
+                'chapter': None
+            }
+
+        book, chapter = attribute.split("\xa0")
+
+        return {
+            'book': book,
+            'chapter': chapter
+        }
